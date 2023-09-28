94abf5bb6555cf588fcf98caa701c973e8ee10bb
MohamedAjimi <r0913462@student.thomasmore.be>
2023-09-28 19:48:31 +0200

    crypt app

--- Hello.py
+++ Hello.py
@@ -1,51 +1,92 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+from simpleai.search import CspProblem, backtrack
 
-LOGGER = get_logger(__name__)
+def solve_cryptarithmetic(word1, word2, result):
+    chars = set(word1 + word2 + result)
+    variables = list(chars)
+    domains = {}
+    for var in variables:
+        domains[var] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+    for word in [word1, word2, result]:
+        first_letter = word[0]
+        domains[first_letter] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+    def constraint_unique(variables, values):
+        value_set = set(values)
+        return len(value_set) == len(values)
 
-    st.write("# Welcome to Streamlit! 👋")
+    def constraint_add(variables, values):
+        var_dict = {}
+        for i in range(len(variables)):
+            var_dict[variables[i]] = values[i]
 
-    st.sidebar.success("Select a demo above.")
+        num1 = ""
+        for char in word1:
+            num1 += str(var_dict[char])
+        num1 = int(num1)
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+        num2 = ""
+        for char in word2:
+            num2 += str(var_dict[char])
+        num2 = int(num2)
+
+        num3 = ""
+        for char in result:
+            num3 += str(var_dict[char])
+        num3 = int(num3)
+
+        return num1 + num2 == num3
+
+    constraints = [
+        (variables, constraint_unique),
+        (variables, constraint_add),
+    ]
+
+    problem = CspProblem(variables, domains, constraints)
+
+    output = backtrack(problem)
+
+    return output
+
+def main():
+   
+    
+
+    st.markdown("# Cryptarithmetic Puzzle Solver :bulb:")
+
+    # Create a sidebar for input widgets
+    with st.sidebar:
+        st.image("crypto.jpg")
+        st.markdown("## Enter the words for the cryptarithmetic puzzle:")
+        word1 = st.text_input("Enter the first word:")
+        word2 = st.text_input("Enter the second word:")
+        result = st.text_input("Enter the result word:")
+        solve_button = st.button("Solve")
+
+    # Display the solution on the main page
+    if solve_button:
+        solution = solve_cryptarithmetic(word1, word2, result)
+        if solution:
+            st.write("Solution found :smile:")
+            st.balloons()
+            equation = f"         {word1}\n+ {word2}\n{'-'*(max(len(word1), len(word2), len(result)) + 2)}\n{result}\n"
+
+            # Construct the solution string
+            word1_solution = ''.join([str(solution[char]) for char in word1])
+            word2_solution = ''.join([str(solution[char]) for char in word2])
+            result_solution = ''.join([str(solution[char]) for char in result])
+            code_block = f"```\n{equation}\n  {word1_solution}\n+ {word2_solution}\n{'-'*(max(len(word1), len(word2), len(result)) + 2)}\n  {result_solution}\n```"
+
+            st.markdown(code_block)
+
+            # Display the letter-number mapping
+            st.markdown("## Letter-Number Mapping:")
+            for char, value in solution.items():
+                st.write(f"{char} equals {value}")
+        else:
+            st.write("No solution found :sob:")
 
 
 if __name__ == "__main__":
-    run()
+    main()
